16cfdc70d46375cedf9a9d89a9679e7b219c8488
trevorswan11 <tcs94@case.edu>
2024-12-05 12:38:16 -0500

    fixed make

--- make.sh
+++ make.sh
@@ -2,11 +2,12 @@
 echo "Creating necessary project files..."
 python3 -m venv myenv
 source myenv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 touch song_history.db
 touch .env
-echo SPOTIFY_CLIENT_ID="your_client_id // TODO" > .env
-echo SPOTIFY_CLIENT_SECRET="your_client_secret // TODO" > .env
+echo SPOTIFY_CLIENT_ID="your_client_id // TODO" >> .env
+echo SPOTIFY_CLIENT_SECRET="your_client_secret // TODO" >> .env
 cd wrapped
 mkdir history
 mkdir mywrapped
