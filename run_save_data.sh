c3d0d6e119b0bfeceb39dd00b4f93717e34e5862
trevorswan11 <tcs94@case.edu>
2024-12-05 12:46:50 -0500

    removed two sh files

--- run_save_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /Users/Trevor/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/Trevor/Projects/Spotify-Tracker
-source myenv/bin/activate
-python save_listening_data.py
-deactivate
