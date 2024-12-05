6a2b46b13514870e4dd51e7da82c2bd73c95d042
trevorswan11 <tcs94@case.edu>
2024-12-05 12:36:09 -0500

    ok its coming

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Creating necessary project files and entering venv..."
+echo "Creating necessary project files..."
 python3 -m venv myenv
 source myenv/bin/activate
+pip install -r requirements.txt
 touch song_history.db
 touch .env
 echo SPOTIFY_CLIENT_ID="your_client_id // TODO" > .env
@@ -9,4 +10,4 @@ echo SPOTIFY_CLIENT_SECRET="your_client_secret // TODO" > .env
 cd wrapped
 mkdir history
 mkdir mywrapped
-echo "All necessary project files created, use deactivate to leave venv..."
+echo "All necessary project files created."
